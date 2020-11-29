ac647d7418f09283b1c5329a979c8017856e8ad3
ashw354 <74320571+ashw354@users.noreply.github.com>
2020-11-29 10:17:08 -0700

    Add files via upload

--- mvc_approx_algos.py
+++ mvc_approx_algos.py
@@ -4,9 +4,7 @@ import random
 from random import shuffle
 from utils import createGraph
 from collections import deque
-
-def isValidVC(VC,G):
-    return all(u in VC or v in VC for u, v in G.edges())
+import numpy as np
 
 def approx_mvc(filename,maxTime=600,seed=10):
     fname = "DATA/"+filename
@@ -14,26 +12,69 @@ def approx_mvc(filename,maxTime=600,seed=10):
     G = createGraph(fname)
 
     # initialize all vertices as not visited
-    visited = [False]*G.nodes()
+    visited = [False for i in range(len(G.nodes()))]
 
-    # Consider all edges one by one
-    for u in range(G.nodes()):
+    startTime = time.time()
+    endTime = startTime + maxTime
+    solTrace = dict()
 
-        # An edge is only picked when
-		# both visited[u] and visited[v]
-		# are false
-        if not visited[u]:
-            for v in G[u]:
-                if not visited[v]:
+    vc_count = 0
 
-                    # Add the vertices (u, v) to the
-					# result set. We make the vertex
-					# u and v visited so that all
-					# edges from/to them would
-					# be ignored
-                    visited[v] = True
-                    visited[u] = True
+    VC = np.zeros((1,len(G.nodes())))
+    print((G.nodes()))
 
-                    break
 
-    return visited
+
+    while time.time()<endTime:
+
+        # Consider all edges one by one
+        for u in range(len(G.nodes())):
+
+
+            # An edge is only picked when
+		    # both visited[u] and visited[v]
+		    # are false
+
+            if not visited[u]:
+
+                for v in G.neighbors(u+1):
+
+                    if v == len(G.nodes()):
+
+                        break
+
+                    if not visited[v]:
+
+                        # Add the vertices (u, v) to the
+					    # result set. We make the vertex
+					    # u and v visited so that all
+					    # edges from/to them would
+					    # be ignored
+                        visited[v] = True
+                        visited[u] = True
+
+                        sol_check = 0
+
+                        for check in range(len(G.nodes())):
+
+                            if visited[check]:
+                                sol_check += 1
+
+                            solTrace[round(time.time()-startTime,2)] = sol_check
+
+
+                        break
+
+        for j in range(len(G.nodes())):
+
+            if visited[j]:
+
+                VC[0,vc_count] = j
+                vc_count += 1
+
+
+        VC_ret = VC[0,0:vc_count]
+
+        break
+
+    return VC_ret, list(solTrace.items())
