c09a2d9a31d0030a44cad224be4ca6caa1592a38
unknown <kprathik@gmail.com>
2020-11-29 16:34:42 -0500

    Fixed bugs

--- utils.py
+++ utils.py
@@ -18,13 +18,13 @@ def isValidVC(VC,G):
 
 def write_to_file(VC,filename,alg,maxtime,seed,solTrace):
     filename = filename.replace(".graph","")
-    fname = "results/"+filename+"_"+alg+"_"+str(maxtime)+"_"+str(seed)+".sol"
+    fname = "output/"+filename+"_"+alg+"_"+str(maxtime)+"_"+str(seed)+".sol"
     VC = list(map(str,VC))
     with open(fname, "w") as f:
         f.write(str(len(VC)) + "\n")
         f.write(str(",".join(list(VC))))
     
-    traceFile = "results/"+filename+"_"+alg+"_"+str(maxtime)+"_"+str(seed)+".trace"
+    traceFile = "output/"+filename+"_"+alg+"_"+str(maxtime)+"_"+str(seed)+".trace"
     with open(traceFile, "w") as f:
         for trace in solTrace:
             line = str(trace[0])+","+str(trace[1])+"\n"
