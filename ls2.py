1ceec23db1478ae90854bfe8187eea102af76210
unknown <kprathik@gmail.com>
2020-12-01 11:22:26 -0500

    Fixed ls2

--- ls2.py
+++ ls2.py
@@ -3,7 +3,7 @@
 This file implements the Simulate Annealing algoritthm to find the minimum vertex cover for an input graph.
 TEAM 22, Aarohi
 Language: Python 3
-Executable: python main.py  -inst jazz.graph -alg ls2 -time 600 -seed 10
+Executable: python main.py  -inst karate.graph -alg ls2 -time 600 -seed 10
 The output is two files: *.sol and *.trace created
 *.sol --- record the size of optimum vertex cover and the nodes in it.
 *.trace --- record all the optimum solution found during the search and the time it was found
@@ -17,50 +17,6 @@ import time
 import os
 import math
 from utils import *
-
-
-def main_ls2(graph_file,cutoff, randSeed):
-    graph_file  = "DATA/"+graph_file
-    randseed = int(randSeed)
-    solution_file =  graph_file[0:-6] + "_LS2_" + str(cutoff) + "_" + str(randseed) + ".sol"
-    trace_file = graph_file[0:-6] + "_LS2_" + str(cutoff) + "_" + str(randseed) + ".trace"
-    output2 = open(trace_file, 'w')
-    G, NumOfVer = read_graph(graph_file)
-    G1 = G.copy()
-    start_time = time.time()
-    sol = initial_solution(G1,graph_file)
-
-    output2.write('%.2f, %i\n' % (0, len(sol)))
-
-    final_solution = sim_ann(G,output2,sol,cutoff,randseed,NumOfVer,start_time,graph_file)
-    size = len(final_solution)
-    total_time = round(time.time() - start_time)
-    # # Write results to output file
-    output1 = open(solution_file, 'w')
-    output1.write(str(size) + '\n')
-    for vid in range(size-1):
-        output1.write(str(final_solution[vid]) + ',')
-        output1.write(str(final_solution[vid+1]))
-
-opt_cutoff = {'DATA/jazz.graph':158, 'DATA/karate.graph':14, 'DATA/football.graph':94, 'DATA/as-22july06.graph':3303, 'DATA/hep-th.graph':3926,'DATA/star.graph':6902, 'DATA/star2.graph':4542, 'DATA/netscience.graph':899, 'DATA/email.graph':594, 'DATA/delaunay_n10.graph':703, 'DATA/power.graph':2203}
-
-# Read the graph file
-def read_graph(filename):
-
-    # Generate a graph to store input information
-    G = nx.Graph()
-    f = open(filename)
-    line = f.readline()  # size of vertices; size of edges; 0
-    values = line.split()
-    values = [int(x) for x in values]
-    NumOfVer, NumOfEdge = values[0], values[1]
-    for i in range(NumOfVer):
-        line = f.readline()    # read graph file line by line
-        values = line.split()
-        values = [int(x) for x in values]
-        for j in range(len(values)):
-            G.add_edge(i+1,values[j])
-    return G,NumOfVer
 
 
 def initial_solution(G, input_file):
@@ -116,5 +72,31 @@ def sim_ann(G, output, sol, cutoff, randseed, NumOfVer,start_time, input_file):
                     if num > prob:    # do not accept modified solution
                         sol = current.copy()
                         uncov_edges = uncov_curr.copy()
-
     return update_sol
+
+def main_ls2(graph_file,cutoff, randSeed):
+    opt_cutoff = {'DATA/jazz.graph':158, 'DATA/karate.graph':14, 'DATA/football.graph':94, 
+    'DATA/as-22july06.graph':3303, 'DATA/hep-th.graph':3926,'DATA/star.graph':6902, 'DATA/star2.graph':4542, 
+    'DATA/netscience.graph':899, 'DATA/email.graph':594, 'DATA/delaunay_n10.graph':703, 'DATA/power.graph':2203}
+
+    filename = graph_file.replace(".graph","")
+    graph_file  = "DATA/"+graph_file
+    randseed = int(randSeed)    
+    solution_file =  "output/"+filename + "_LS2_" + str(cutoff) + "_" + str(randseed) + ".sol"
+    trace_file = "output/"+filename + "_LS2_" + str(cutoff) + "_" + str(randseed) + ".trace"
+    output2 = open(trace_file, 'w')
+    G = createGraph(graph_file)
+    NumOfVer = G.number_of_nodes()
+    G1 = G.copy()
+    start_time = time.time()
+    sol = initial_solution(G1,graph_file)
+    output2.write('%.2f, %i\n' % (0, len(sol)))
+    final_solution = sim_ann(G,output2,sol,cutoff,randseed,NumOfVer,start_time,graph_file)
+    size = len(final_solution)
+    total_time = round(time.time() - start_time)
+    # # Write results to output file
+    output1 = open(solution_file, 'w')
+    output1.write(str(size) + '\n')
+    for vid in range(size-1):
+        output1.write(str(final_solution[vid]) + ',')
+        output1.write(str(final_solution[vid+1]))
