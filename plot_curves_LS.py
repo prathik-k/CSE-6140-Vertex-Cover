5dddc2db8b7dd89285c0529e0ccf22ae2ebda485
unknown <kprathik@gmail.com>
2020-12-04 13:07:13 -0500

    Added qrtd and sqd plots

--- plot_curves_LS.py
+++ plot_curves_LS.py
@@ -10,7 +10,7 @@ For the power.graph instance, the LS1 (hill climbing) algorithm yielded an avera
 We plot the QRTD plots for the above cases, with the time ranging from 0-17s.
 '''
 
-def plot_qrtd(inst='power',maxtime=600,seeds=[10,20,30,40,50,60,70,80,90,100],method="LS1",perc_sols = [5,8,10,15,25]):    
+def plot_qrtd(inst='power',maxtime=600,seeds=[10,20,30,40,50,60,70,80,90,100],method="LS1",perc_sols = [6,8,15,20]):    
     if inst=='power':
         q = 2203
     elif inst=='star2':
@@ -36,14 +36,25 @@ def plot_qrtd(inst='power',maxtime=600,seeds=[10,20,30,40,50,60,70,80,90,100],me
     for perc in perc_sols:
         target_size = int(q*(100+perc)/100)
         target_sizes[perc] = target_size 
-    xvals = np.arange(mt)
+
+
+    if inst=="power" and method=="LS2":
+        xvals = [0.1,0.5,2,5]    
+    elif inst=="power" and method=="LS1":
+        xvals = [0,2,4,6,8,10,12,20]
+    elif inst=="star2" and method=="LS2":
+        xvals = [50,80,100,200]
+    elif inst=="star2" and method=="LS1":
+        xvals = [450,470,500,550]
 
     for case in all_cases:
         for t in xvals:
-            case[int(t)] = case.get(t, case[min(case.keys(), key=lambda k: abs(k-t))])
+            case[int(t)] = case.get(t, case[min(case.keys(), key=lambda k: abs(k-t))])    
 
     fig, ax = plt.subplots()
-    for perc in perc_sols:        
+    markers = [("o",11),("^",9),("D",7),("H",5)]
+    for i,perc in enumerate(perc_sols):     
+        mk,size = markers[i]   
         P_vals = []
         for t in xvals:
             num_success,num_total = 0,len(seeds)
@@ -51,15 +62,15 @@ def plot_qrtd(inst='power',maxtime=600,seeds=[10,20,30,40,50,60,70,80,90,100],me
                 if case[int(t)]<=target_sizes[perc]:
                     num_success+=1
             P_vals.append(num_success/num_total)
-        ax.plot(P_vals,label=str(perc)+"%")
-        ax.set_ylim([0,1])
+        ax.plot(xvals,P_vals,marker=mk,markersize=size,label=str(perc)+"%")
+        ax.set_ylim([0,1.1])
     ax.set_ylabel("P(solve)")
     ax.set_xlabel("Time (s)")
     ax.legend()
-    plt.savefig("plots/"+method+"_qrtd.png")
+    plt.savefig("plots/"+inst+"_"+method+"_qrtd.png")
     plt.clf()  
 
-def plot_sqd(inst='power',maxtime=600,seeds=[10,20,30,40,50,60,70,80,90,100],method="LS1",perc_sols = np.linspace(0,25,21)):
+def plot_sqd(inst='power',maxtime=600,seeds=[10,20,30,40,50,60,70,80,90,100],method="LS1",perc_sols = np.linspace(0,15,21)):
     if inst=='power':
         q = 2203
     elif inst=='star2':
@@ -80,7 +91,15 @@ def plot_sqd(inst='power',maxtime=600,seeds=[10,20,30,40,50,60,70,80,90,100],met
         target_size = int(q*(100+perc)/100)
         target_sizes[perc] = target_size     
 
-    times = np.round(np.linspace(2,mt,num=5),2)
+    if inst=="power" and method=="LS2":
+        times = [1,2,5,10]    
+    elif inst=="power" and method=="LS1":
+        times = [6,8,10,12]
+    elif inst=="star2" and method=="LS2":
+        times = [100,150,200,300]
+    elif inst=="star2" and method=="LS1":
+        times = [450,470,500,550]
+
     for case in all_cases:
         for t in times:
             case[t] = case.get(t, case[min(case.keys(), key=lambda k: abs(k-t))])
@@ -90,7 +109,9 @@ def plot_sqd(inst='power',maxtime=600,seeds=[10,20,30,40,50,60,70,80,90,100],met
         all_cases[i] = case
     
     fig, ax = plt.subplots()
-    for t in times:
+    markers = [("o",11),("^",9),("D",7),("H",5)]
+    for i,t in enumerate(times):
+        mk,size = markers[i]  
         P_vals = []
         for perc in perc_sols:
             num_success,num_total = 0,len(seeds)
@@ -98,20 +119,25 @@ def plot_sqd(inst='power',maxtime=600,seeds=[10,20,30,40,50,60,70,80,90,100],met
                 if case[t]<=target_sizes[perc]:
                     num_success+=1
             P_vals.append(num_success/num_total)
-        ax.plot(perc_sols,P_vals,label=str(t)+"s")
-        ax.set_ylim([0,1])
+        ax.plot(perc_sols,P_vals,marker=mk,markersize=size,label=str(t)+"s")
+        ax.set_ylim([0,1.1])
     ax.set_ylabel("P(solve)")
     ax.set_xlabel("Solution quality (%)")
     ax.legend()
-    plt.savefig("plots/"+method+"_sqd.png")
+    plt.savefig("plots/"+inst+"_"+method+"_sqd.png")
     plt.clf()   
 
 
 if __name__=="__main__":
     plot_qrtd(inst="star2",maxtime=1000,method="LS1")
     plot_qrtd(inst="star2",maxtime=1000,method="LS2")
+    plot_qrtd(inst="power",maxtime=1000,method="LS2")
+    plot_qrtd(inst="power",maxtime=1000,method="LS1")
+
     plot_sqd(inst="power",maxtime=1000,method="LS1")
     plot_sqd(inst="power",maxtime=1000,method="LS2")
+    plot_sqd(inst="star2",maxtime=1000,method="LS1")
+    plot_sqd(inst="star2",maxtime=1000,method="LS2")
     
 
 
